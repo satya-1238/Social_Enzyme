1b82a7788e1418c6c0e88fde8a1c31c1933a036e
Satya <satyaprakashjnv6789@gmail.com>
2021-12-25 12:27:49 +0530

    set Up of viewEngine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port=8000;
 // Use Express Router
 app.use('/',require('./routes')); //by access index.js inside routers
 
+//set up the view Engine;
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
  if(err)
  {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
-const { Router } = require('express');
+// const { Router } = require('express');
 const express=require('express');
 const router=express.Router();
+
+
+
 const homeController=require('../controllers/home_controller');
 console.log("Router Loaded");
+
+
+
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+// for any further routes ,access from here
+// router.use('/routerName,require('./routerFile));
+
+
 module.exports=router;
